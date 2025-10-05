af8ae752e1f17eed713d17979900067d25bdba8d
DpiTStudio <admin@dpitstudio.ru>
2025-10-05 23:50:49 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   mysite/accounts/static/accounts/css/accounts.css
    	new file:   mysite/accounts/static/accounts/css/login.css
    	new file:   mysite/accounts/static/accounts/css/profile.css
    	new file:   mysite/accounts/static/accounts/css/register.css
    	new file:   mysite/accounts/static/accounts/css/tickets.css
    	modified:   mysite/accounts/templates/accounts/create_ticket.html
    	modified:   mysite/accounts/templates/accounts/login.html
    	modified:   mysite/accounts/templates/accounts/password_change.html
    	modified:   mysite/accounts/templates/accounts/profile.html
    	modified:   mysite/accounts/templates/accounts/profile_edit.html
    	modified:   mysite/accounts/templates/accounts/profile_update.html
    	modified:   mysite/accounts/templates/accounts/register.html
    	modified:   mysite/accounts/templates/accounts/ticket_detail.html
    	modified:   mysite/accounts/templates/accounts/ticket_list.html
    	modified:   mysite/db.sqlite3
    	modified:   mysite/debug.log
    	modified:   mysite/main/__pycache__/models.cpython-310.pyc
    	new file:   mysite/main/migrations/0006_alter_sitesettings_options_and_more.py
    	new file:   mysite/main/migrations/__pycache__/0006_alter_sitesettings_options_and_more.cpython-310.pyc
    	modified:   mysite/main/templates/main/index.html
    	modified:   mysite/reviews/__pycache__/models.cpython-310.pyc
    	new file:   mysite/reviews/migrations/0004_remove_review_id_review_author_review_id_review_and_more.py
    	new file:   mysite/reviews/migrations/0005_alter_review_author.py
    	new file:   mysite/reviews/migrations/__pycache__/0004_remove_review_id_review_author_review_id_review_and_more.cpython-310.pyc
    	new file:   mysite/reviews/migrations/__pycache__/0005_alter_review_author.cpython-310.pyc
    	modified:   mysite/reviews/models.py
    	modified:   mysite/templates/base.html
    
    Changes not staged for commit:
    	modified:   mysite/debug.log

--- mysite/reviews/models.py
+++ mysite/reviews/models.py
@@ -2,6 +2,7 @@
 
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 
 
 class Review(models.Model):
@@ -11,13 +12,22 @@ class Review(models.Model):
         ("rejected", "Отклонен"),
     )
 
+    author = models.ForeignKey(
+        User,
+        verbose_name=_("Пользователь"),
+        on_delete=models.CASCADE,
+        related_name="reviews",
+        null=True,  # если автор может быть не указан
+        blank=True,  # если поле может быть пустым в формах
+    )
+    email = models.EmailField(_("Email"), max_length=254, blank=True)
     full_name = models.CharField(_("ФИО"), max_length=100)
+    message = models.TextField(_("Сообщение"))
     phone = models.CharField(_("Телефон"), max_length=20)
-    email = models.EmailField(_("Email"))
-    message = models.TextField(_("Сообщение"), blank=True)
     status = models.CharField(
         _("Статус"), max_length=10, choices=STATUS_CHOICES, default="pending"
     )
+    id_review = models.AutoField(primary_key=True)
     created_at = models.DateTimeField(_("Создано"), auto_now_add=True)
     updated_at = models.DateTimeField(_("Обновлено"), auto_now=True)
 
