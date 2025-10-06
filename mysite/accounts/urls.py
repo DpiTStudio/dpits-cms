abe20e2b35ddb112f8f7a7b35370e6d1b98c531a
DpiTStudio <admin@dpitstudio.ru>
2025-10-07 00:52:51 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   mysite/accounts/__pycache__/admin.cpython-310.pyc
    	modified:   mysite/accounts/__pycache__/forms.cpython-310.pyc
    	modified:   mysite/accounts/__pycache__/models.cpython-310.pyc
    	modified:   mysite/accounts/__pycache__/urls.cpython-310.pyc
    	modified:   mysite/accounts/__pycache__/views.cpython-310.pyc
    	modified:   mysite/accounts/admin.py
    	modified:   mysite/accounts/forms.py
    	new file:   mysite/accounts/migrations/0005_alter_ticket_user_alter_userprofile_avatar_and_more.py
    	new file:   mysite/accounts/migrations/__pycache__/0005_alter_ticket_user_alter_userprofile_avatar_and_more.cpython-310.pyc
    	modified:   mysite/accounts/models.py
    	modified:   mysite/accounts/static/accounts/css/accounts.css
    	modified:   mysite/accounts/templates/accounts/logout_confirm.html
    	modified:   mysite/accounts/urls.py
    	modified:   mysite/accounts/views.py
    	modified:   mysite/db.sqlite3
    	modified:   mysite/debug.log

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -11,21 +11,22 @@ urlpatterns = [
     path(
         "login/",
         auth_views.LoginView.as_view(
-            template_name="accounts/login.html", redirect_authenticated_user=True
+            template_name="accounts/login.html",
+            redirect_authenticated_user=True,
         ),
         name="login",
     ),
     path(
         "logout/",
-        auth_views.LogoutView.as_view(
-            template_name="accounts/logout.html", next_page="/"
-        ),
+        views.custom_logout,
         name="logout",
     ),
     path("logout/confirm/", views.logout_confirmation, name="logout_confirm"),
     # Профиль пользователя
     path("profile/", views.profile, name="profile"),
-    path("profile/edit/", views.profile_edit, name="profile_edit"),
+    path(
+        "profile/edit/", views.profile_edit, name="profile_edit"
+    ),  # ДОБАВЛЕНО: недостающий маршрут
     path("profile/update/", views.profile_update, name="profile_update"),
     path("password/change/", views.password_change, name="password_change"),
     # Тикеты
@@ -33,8 +34,3 @@ urlpatterns = [
     path("tickets/create/", views.create_ticket, name="create_ticket"),
     path("tickets/<int:pk>/", views.ticket_detail, name="ticket_detail"),
 ]
-
-handler400 = "main.views.handler400"
-handler403 = "main.views.handler403"
-handler404 = "main.views.handler404"
-handler500 = "main.views.handler500"
