0c6610d9a365a6164097b8026909b110aded40a8
DpiTStudio <admin@dpitstudio.ru>
2025-10-13 21:24:01 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   mysite/accounts/__pycache__/models.cpython-310.pyc
    	modified:   mysite/accounts/models.py
    	modified:   mysite/accounts/static/accounts/css/tickets.css
    	modified:   mysite/db.sqlite3
    	modified:   mysite/debug.log
    	modified:   mysite/main/static/main/css/styles.css
    	modified:   mysite/staticfiles/accounts/css/tickets.css
    	modified:   mysite/staticfiles/css/base.css
    	modified:   mysite/staticfiles/css/components.css
    	modified:   mysite/staticfiles/css/pages.css
    	modified:   mysite/staticfiles/css/style.css
    	modified:   mysite/staticfiles/main/css/styles.css
    	modified:   mysite/staticfiles/portfolio/css/categories.css
    	modified:   mysite/staticfiles/portfolio/css/forms.css
    	modified:   mysite/staticfiles/portfolio/css/order_detail.css
    	modified:   mysite/staticfiles/portfolio/css/portfolio_detail.css
    	modified:   mysite/staticfiles/portfolio/css/portfolio_list.css

--- mysite/accounts/models.py
+++ mysite/accounts/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.urls import reverse
+
+# from django.urls import reverse
 import os
 
 
