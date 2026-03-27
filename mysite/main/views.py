892521946f7288fe30fc636fc1a966349bb19162
DpiTStudio <admin@dpitstudio.ru>
2026-03-28 00:58:23 +0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   mysite/db.sqlite3
    	modified:   mysite/main/__pycache__/views.cpython-314.pyc
    	modified:   mysite/main/templates/main/home.html
    	deleted:    mysite/main/templates/main/index.html
    	modified:   mysite/main/views.py

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -208,7 +208,7 @@ class IndexView(MaintenanceMixin, BaseView):
     Наследует функциональность обслуживания и базовые данные.
     """
 
-    template_name = "main/index.html"
+    template_name = "main/home.html"
 
     def get_context_data(self, **kwargs):
         """
